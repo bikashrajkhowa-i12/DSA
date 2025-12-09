0264f54357e2f3b762313ce5eb3ad59408c7f2c1
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2025-12-09 22:57:22 +0530

    feat: :sparkles: ++hashing-> sample problem

--- core/hashing/04_program.cpp
+++ core/hashing/04_program.cpp
@@ -1,7 +1,7 @@
 /*
-Given an array nums of n integers, find the most frequent element 
-in it i.e., the element that occurs the maximum number of times. 
-If there are multiple elements that appear a maximum number of times, 
+Given an array nums of n integers, find the most frequent element
+in it i.e., the element that occurs the maximum number of times.
+If there are multiple elements that appear a maximum number of times,
 find the smallest of them.
 
 Eg1 :
@@ -12,3 +12,46 @@ Eg2:
 Input: nums = [4, 4, 5, 5, 6]
 Output: 4
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter array size: ";
+	cin >> n;
+
+	int arr[n];					// input elements
+	unordered_map<int, int> mp; // store (key, frequency) in unordered_map
+
+	cout << "Enter " << n << " elements: ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		mp[arr[i]] += 1;
+	}
+
+	// frequent element counter
+	int maxCount = 0;
+	int res = 0;
+
+	// loop over the map
+	for (auto &it : mp)
+	{
+		cout << it.first << " -> " << it.second << endl;
+
+		int num = it.first;
+		int freq = it.second;
+
+		// update frequent element
+		if (freq > maxCount || (freq == maxCount && num < res))
+		{
+			maxCount = freq;
+			res = num;
+		}
+	}
+
+	cout << "The most frequent element is: " << res << endl;
+	return 0;
+}
