554f4d1391b44e14adb4ce5c197adf1b0fa4aafe
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-01-27 13:57:18 +0530

    feat: :sparkles: create a doubly LL

--- core/linked-list/doubly-linked-list/01_create_doubly_LL.cpp
+++ core/linked-list/doubly-linked-list/01_create_doubly_LL.cpp
@@ -23,36 +23,67 @@ public:
     }
 };
 
+Node *covert_to_2d_LL(vector<int> &arr)
+{
+    if (arr.size() == 0)
+        return NULL;
+
+    Node *head = new Node(arr[0]);
+    Node *curr = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *newNode = new Node(arr[i]);
+        curr->next = newNode;
+        newNode->prev = curr;
+        curr = newNode;
+    }
+    return head;
+}
+
 void print_2d_LL(Node *head)
 {
+    if (head == NULL)
+    {
+        cout << "List is empty\n";
+        return;
+    }
+
     Node *temp = head;
+    Node *tail = NULL;
 
     cout << "Traverse forward: ";
-    while (temp->next != NULL)
+    while (temp != NULL)
     {
-        cout << temp->data << "->";
+        cout << temp->data;
+        if (temp->next)
+            cout << "->";
+        tail = temp; // save last valid node
         temp = temp->next;
     }
-    cout << temp->data; // the last node
 
     cout << endl
          << "Traverse backward: ";
-    while (temp->prev != NULL)
+    temp = tail; // restore from tail
+    while (temp != NULL)
     {
-        cout << temp->data << "<-";
+        cout << temp->data;
+        if (temp->prev)
+            cout << "->";
         temp = temp->prev;
     }
-    cout << temp->data; // the first node
 }
 
 int main()
 {
-    Node *head = new Node(10);
-    head->next = new Node(11);
-    head->next->next = new Node(12);
+    // Node *head = new Node(10);
+    // head->next = new Node(11);
+    // head->next->next = new Node(12);
+    // head->next->next->prev = head->next;
+    // head->next->prev = head;
 
-    head->next->next->prev = head->next;
-    head->next->prev = head;
+    vector<int> arr = {1, 2, 3, 4, 5};
+    Node *head = covert_to_2d_LL(arr);
 
     print_2d_LL(head);
 }
