41570993be8343ef915bee2e47f1acb16643f5c0
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-03-03 14:01:54 +0530

    feat: :sparkles: rearrange elements by sign (optimized) ++

--- core/06_arrays/medium/10_rearrange_elements_by_sign.cpp
+++ core/06_arrays/medium/10_rearrange_elements_by_sign.cpp
@@ -35,28 +35,68 @@ vector<int> brute_rearrange_by_sign(vector<int> &nums)
     }
 
     vector<int> res(nums.size());
-    bool startPos = (nums[0] > 0);
+    // bool startPos = (nums[0] > 0); since the start is always a positive integer
 
     for (int i = 0; i < nums.size() / 2; i++)
     {
-        if (startPos)
-        {
-            res[2 * i] = positives[i];
-            res[2 * i + 1] = negatives[i];
-        }
-        else
-        {
-            res[2 * i] = negatives[i];
-            res[2 * i + 1] = positives[i];
-        }
+        // if (startPos)
+        // {
+        res[2 * i] = positives[i];
+        res[2 * i + 1] = negatives[i];
+        // }
+        // else
+        // {
+        //     res[2 * i] = negatives[i];
+        //     res[2 * i + 1] = positives[i];
+        // }
     }
 
     return res;
 }
 
+/*
+Optimal approach:
+- Skip the separation step entirely.
+- place elements directly into their correct index in one pass
+- using two pointers to track the next position of the current sign index.
+
+TC: O(n); SC: O(n)
+-   Both approaches are O(n) time and O(n) space, but the optimal solution runs in a single pass
+    with no intermediate vectors so the constant factor is smaller.
+-   In practice this means better cache performance and less memory allocation overhead, which matters at scale.
+*/
+vector<int> optimal_rearrange_elements_by_sign(vector<int> &nums)
+{
+    vector<int> result(nums.size());
+
+    // if starting sign is +ve
+    int posIdx = 0;
+    int negIdx = 1;
+
+    // if starting sign is preserved
+    // int posIdx = nums[0] > 0 ? 0 : 1;
+    // int negIdx = 1 - posIdx;
+
+    for (int x : nums)
+    {
+        if (x > 0)
+        {
+            result[posIdx] = x;
+            posIdx += 2;
+        }
+        else
+        {
+            result[negIdx] = x;
+            negIdx += 2;
+        }
+    }
+
+    return result;
+}
+
 int main()
 {
-    vector<int> nums = {1, 2, -4, -5};
+    vector<int> nums = {-3, 1, 2, -4, -5, 6};
     cout << "Given array: [";
     for (int i = 0; i < nums.size(); i++)
     {
@@ -67,8 +107,9 @@ int main()
     }
 
     cout << endl
-         << "Rearraged elements by sign (brute): [";
-    vector<int> res = brute_rearrange_by_sign(nums);
+         << "Rearraged elements by sign: [";
+    // vector<int> res = brute_rearrange_by_sign(nums);
+    vector<int> res = optimal_rearrange_elements_by_sign(nums);
 
     for (int i = 0; i < res.size(); i++)
     {
