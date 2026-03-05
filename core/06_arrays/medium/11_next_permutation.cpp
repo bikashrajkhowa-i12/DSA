27a71f0a4f56573be1e0d0ba6ec28b082550a529
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-03-05 13:06:43 +0530

    feat: :sparkles: next permutation(optimal approach)

--- core/06_arrays/medium/11_next_permutation.cpp
+++ core/06_arrays/medium/11_next_permutation.cpp
@@ -17,7 +17,6 @@ Given an array of integers nums, find the next permutation of nums.
 #include <bits/stdc++.h>
 using namespace std;
 
-// ({1,2,3}, 0, {})
 void permute(vector<int> &nums, int start, vector<vector<int>> &allPerms)
 {
     if (start == nums.size())
@@ -48,7 +47,7 @@ void brute_next_permutation(vector<int> &arr)
 
     // 1. generate all permutations
     vector<int> sorted_nums = arr;                // {2,1,3}
-    sort(sorted_nums.begin(), sorted_nums.end()); // {1,2,3}
+    sort(sorted_nums.begin(), sorted_nums.end()); // {1,2,3} else we would miss permutations that comes before it.
     permute(sorted_nums, 0, allPerms);            // ({1,2,3}, 0, {})
 
     // 2. sort them lexiologically
@@ -65,13 +64,55 @@ void brute_next_permutation(vector<int> &arr)
     }
 }
 
+/* Optimal approach
+    - Find dip   → scan from right, find first nums[i] < nums[i+1]
+    - Swap       → scan from right, find first nums[i] > nums[pivot], swap arr[i] and arr[pivot]
+    - Reverse    → reverse everything after index i
+
+    Complexity:
+        - TC: O(n) + O(n) = O(2n) => O(n);
+        - SC: O(1)
+*/
 void optimal_next_permutation(vector<int> &arr)
 {
+    int pivot = -1;
+
+    // find the pivot(dip)
+    for (int i = arr.size() - 2; i >= 0; i--)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            pivot = i;
+            break;
+        }
+    }
+
+    // reverse if already at the max permutation
+    if (pivot == -1)
+    {
+        reverse(arr.begin(), arr.end());
+        return;
+    }
+
+    // swap first greater element than pivot from right
+    for (int i = arr.size() - 1; i >= 0; i--)
+    {
+        if (arr[i] > arr[pivot])
+        {
+            swap(arr[i], arr[pivot]);
+            break;
+        }
+    }
+
+    /* at this point array will be sorted descending for sure
+        reverse everything after pivot + 1
+    */
+    reverse(arr.begin() + pivot + 1, arr.end());
 }
 
 int main()
 {
-    vector<int> arr = {2, 1, 3};
+    vector<int> arr = {5, 1, 4, 3, 2};
     cout << " Given array: [";
     for (int i = 0; i < arr.size(); i++)
     {
@@ -88,11 +129,11 @@ int main()
 
     /* Brute-force next_permutation
      */
-    brute_next_permutation(arr);
+    // brute_next_permutation(arr);
 
     /* Optimal next_permutation
      */
-    // optimal_next_permutation(arr);
+    optimal_next_permutation(arr);
 
     cout
         << endl
