6d246822bffca2f3a76ccb00f33e0d8aa45d14eb
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-03-18 12:47:30 +0530

    feat: :sparkles: kth missing positive number(brute)

---
+++ core/07_binary-search/bs-on-answers/07_kth_missing_positive_number.cpp
@@ -0,0 +1,76 @@
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+
+## Example 1:
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+## Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void print_arr(const vector<int> &arr)
+{
+    int n = arr.size();
+    if (n == 0)
+    {
+        cout << "[]";
+        return;
+    }
+
+    cout << "Given arr: [";
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+            cout << arr[i] << "]";
+        else
+            cout << arr[i] << ", ";
+    }
+    return;
+}
+
+/* brute-force
+    TC: O(n * k)
+    SC: O(1)
+
+*/
+int brute_missing_no(vector<int> &arr, int k)
+{
+    int num = 1;
+    int missingCount = 0;
+
+    while (true)
+    {
+        // check if num is NOT in array
+        if (find(arr.begin(), arr.end(), num) == arr.end())
+        {
+            missingCount++;
+            if (missingCount == k)
+                return num;
+        }
+        num++;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4};
+    int k = 2;
+    print_arr(arr);
+    cout << endl
+         << "k: " << k;
+    cout << endl
+         << "The " << k << "th missing +ve number is: " << brute_missing_no(arr, k);
+}
