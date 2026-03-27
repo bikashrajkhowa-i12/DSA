b214f7c55d8049a99648ae9de306221f3980b315
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-03-27 13:45:52 +0530

    feat: :zap: kth element of two sorted arrays(optimzied)

--- core/07_binary-search/bs-on-answers/14_kth_element_of_two_sorted_arrays.cpp
+++ core/07_binary-search/bs-on-answers/14_kth_element_of_two_sorted_arrays.cpp
@@ -83,6 +83,52 @@ int brute_kth_element(vector<int> &arr1, vector<int> &arr2, int k)
     return res[k - 1];
 }
 
+/* Optimized
+    range: k
+    TC: O(log k)
+    SC: O(1)
+*/
+// Core intuition: Left partition must contain k elements total.
+int optimized_kth_element(vector<int> &arr1, vector<int> &arr2, int k)
+{
+    int n = arr1.size();
+    int m = arr2.size();
+
+    // Make sure arr1 is the smaller array
+    if (n > m)
+        return optimized_kth_element(arr2, arr1, k);
+
+    int low = max(0, k - m);
+    int high = min(k, n);
+
+    while (low <= high)
+    {
+        int cut1 = (low + high) / 2;
+        int cut2 = k - cut1;
+
+        int L1 = (cut1 == 0) ? INT_MIN : arr1[cut1 - 1];
+        int L2 = (cut2 == 0) ? INT_MIN : arr2[cut2 - 1];
+
+        int R1 = (cut1 == n) ? INT_MAX : arr1[cut1];
+        int R2 = (cut2 == m) ? INT_MAX : arr2[cut2];
+
+        if (L1 <= R2 && L2 <= R1)
+        {
+            return max(L1, L2);
+        }
+        else if (L1 > R2)
+        {
+            high = cut1 - 1;
+        }
+        else
+        {
+            low = cut1 + 1;
+        }
+    }
+
+    return -1; // This should never happen if k is valid
+}
+
 int main()
 {
     vector<int> nums1 = {100, 112, 256, 349, 770};
@@ -91,5 +137,8 @@ int main()
     print_arr(nums1, nums2);
     cout << endl
          << "k: " << k;
-    cout << "Kth element(brute): " << brute_kth_element(nums1, nums2, k);
+    cout << endl
+         << "kth element(brute): " << brute_kth_element(nums1, nums2, k);
+    cout << endl
+         << "kth element(optimized): " << optimized_kth_element(nums1, nums2, k);
 }
