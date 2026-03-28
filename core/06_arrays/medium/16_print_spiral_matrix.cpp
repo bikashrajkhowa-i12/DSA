c396cc4fe0681f0d4528040d11e8275e8dfc06f5
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-03-28 13:29:04 +0530

    feat: :zap: Spiral matrix: ++improved edge cases performance

--- core/06_arrays/medium/16_print_spiral_matrix.cpp
+++ core/06_arrays/medium/16_print_spiral_matrix.cpp
@@ -21,32 +21,6 @@ Example 2:
 #include <vector>
 using namespace std;
 
-void print_2d_matrix(const vector<vector<int>> &mat)
-{
-    int n = mat.size();
-
-    if (n == 0)
-        cout << "[]";
-
-    cout << "[\n";
-    for (int i = 0; i < n; i++)
-    {
-        cout << "[";
-        for (int j = 0; j < n; j++)
-        {
-            if (j == n - 1)
-                cout << mat[i][j] << "]";
-            else
-                cout << mat[i][j] << ",";
-        }
-        if (i == n - 1)
-            cout << ",\n]";
-        else
-            cout << ", \n";
-    }
-    return;
-}
-
 void print_matrix(vector<int> arr)
 {
     int n = arr.size();
@@ -67,37 +41,79 @@ void print_matrix(vector<int> arr)
     return;
 }
 
+void print_2d_matrix(const vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    int m = mat[0].size();
+
+    if (n == 0)
+        cout << "[]";
+    else if (n == 1)
+        return print_matrix(mat[0]);
+
+    cout << "[\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "[";
+        for (int j = 0; j < m; j++)
+        {
+            if (j == m - 1)
+                cout << mat[i][j] << "]";
+            else
+                cout << mat[i][j] << ",";
+        }
+        if (i == n - 1)
+            cout << ",\n]";
+        else
+            cout << ", \n";
+    }
+    return;
+}
+
 /*
     TC: O(n * m) // touching each and every element
-    SC: O(n * m) // required space for all the element in matrix
+    SC: O(n * m) // storing all elements in spiral order
 */
 vector<int> spiral_matrix(vector<vector<int>> &mat)
 {
-    int n = mat.size();    // cols length
-    int m = mat[0].size(); // rows length
-
     vector<int> spiralMat;
+    if (mat.empty())
+        return spiralMat;
 
-    int left = 0, right = m - 1, top = 0, bottom = n - 1;
+    int m = mat.size();    // number of rows
+    int n = mat[0].size(); // number of columns
+
+    int left = 0, right = n - 1, top = 0, bottom = m - 1;
 
     while (left <= right && top <= bottom)
     {
+        // traverse top row
         for (int i = left; i <= right; i++)
             spiralMat.push_back(mat[top][i]);
         top++;
 
+        // traverse right column
         for (int i = top; i <= bottom; i++)
             spiralMat.push_back(mat[i][right]);
         right--;
 
-        for (int i = right; i >= left; i--)
-            spiralMat.push_back(mat[bottom][i]);
-        bottom--;
+        // traverse bottom row if still within bounds
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+                spiralMat.push_back(mat[bottom][i]);
+            bottom--;
+        }
 
-        for (int i = bottom; i >= top; i--)
-            spiralMat.push_back(mat[i][left]);
-        left++;
+        // traverse left column if still within bounds
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+                spiralMat.push_back(mat[i][left]);
+            left++;
+        }
     }
+
     return spiralMat;
 }
 
