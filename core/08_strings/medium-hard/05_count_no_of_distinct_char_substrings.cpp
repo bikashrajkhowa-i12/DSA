cf01620ae8159f1b0b1ba157047aee8bc0db0ac2
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-03-31 23:36:45 +0530

    feat: :zap: count no of distinct k substrings(optimized)

--- core/08_strings/medium-hard/05_count_no_of_distinct_char_substrings.cpp
+++ core/08_strings/medium-hard/05_count_no_of_distinct_char_substrings.cpp
@@ -63,12 +63,57 @@ int countSubstringsExactlyK(string s, int k)
     return total;
 }
 
+long long atMostK(const string &s, int k)
+{
+    if (k < 0)
+        return 0;
+
+    vector<int> freq(256, 0);
+    int left = 0, distinct = 0;
+    long long count = 0;
+
+    for (int right = 0; right < s.size(); right++)
+    {
+        if (freq[s[right]] == 0)
+            distinct++;
+
+        freq[s[right]]++;
+
+        while (distinct > k)
+        {
+            freq[s[left]]--;
+            if (freq[s[left]] == 0)
+                distinct--;
+            left++;
+        }
+
+        count += (right - left + 1);
+    }
+
+    return count;
+}
+
+/* Optimal
+    - Idea-Intuition:
+        - Exactly(k) = AtMost(k) - AtMost(k-1)
+        - Use sliding window to count substrings with at most k distinct by adding (right - left + 1) for each right.
+    - TC: O(n)
+    - SC: O(n)
+*/
+int optimal_countSubstringsExactlyK(string s, int k)
+{
+    return atMostK(s, k) - atMostK(s, k - 1);
+}
+
 int main()
 {
     string s = "pqpqs";
     int k = 2;
     cout << "Given string: " << s;
     cout << endl
-         << "Total count:"
-         << countSubstringsExactlyK(s, k) << endl;
+         << "Total count(brute): "
+         << countSubstringsExactlyK(s, k);
+    cout << endl
+         << "Total count(optimal): "
+         << optimal_countSubstringsExactlyK(s, k);
 }
